c1cf5ee36d618adfe9bd0e9101d9201330500ab4
D. Nagy Gergő <hello@iamgergo.com>
2025-12-09 08:59:17 +0100

    wip

---
+++ src/Client.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cone\Vies;
+
+class Vies
+{
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
