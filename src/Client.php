b3c9297d5442c0ce5d2a8bff8e755b72ba7e7d89
D. Nagy Gergő <hello@iamgergo.com>
2025-12-09 18:47:54 +0100

    wip

--- src/Client.php
+++ src/Client.php
@@ -14,28 +14,10 @@ class Client
     protected PublicApi $api;
 
     /**
-     * Create a new client instance.
-     */
-    public function __construct()
-    {
-        $this->config()->setHost('https://ec.europa.eu/taxation_customs/vies/rest-api');
-    }
-
-    /**
      * Get the public API instance.
      */
     public function api(): PublicApi
     {
-        return $this->api ??= new PublicApi(
-            config: $this->config()
-        );
-    }
-
-    /**
-     * Get the configuration instance.
-     */
-    protected function config(): Configuration
-    {
-        return Configuration::getDefaultConfiguration();
+        return $this->api ??= new PublicApi;
     }
 }
