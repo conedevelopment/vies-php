7d116df2b7046586423372eca0ef2572f20ab335
D. Nagy Gergő <hello@iamgergo.com>
2025-12-09 16:42:35 +0100

    wip

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,21 +8,22 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
     ->exclude('vendor')
     ->exclude('test')
-    ->exclude('tests');
+    ->exclude('tests')
+;
 
 $config = new PhpCsFixer\Config();
-
 return $config->setRules([
         '@PSR12' => true,
         'phpdoc_order' => true,
-        'array_syntax' => [ 'syntax' => 'short' ],
+        'array_syntax' => ['syntax' => 'short'],
+        'single_line_comment_style' => ['comment_types' => ['hash']],
         'strict_comparison' => true,
         'strict_param' => true,
-        'no_trailing_whitespace' => false,
-        'no_trailing_whitespace_in_comment' => false,
-        'braces' => false,
-        'single_blank_line_at_eof' => false,
-        'blank_line_after_namespace' => false,
-        'no_leading_import_slash' => false,
+        'phpdoc_indent' => true,
+        'phpdoc_no_package' => true,
+        'phpdoc_scalar' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
+        'declare_strict_types' => true,
     ])
-    ->setFinder($finder);
+    ->setFinder($finder)
+;
